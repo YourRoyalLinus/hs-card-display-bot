cd11c1cc8f8220d732f4be918c48340425c55bcc
YourRoyalLinus <52103944+YourRoyalLinus@users.noreply.github.com>
2022-02-17 23:33:02 -0500

    Updated loading env variables
        - Will now work when executing bot library as a script
            - Will use py -m "botname" to run the bot
            - This required checking for .env based on the abs path of
            the _api.py file
        - Added error handling to check that necessary env vars exist
            - Will now raise APIException if .env can't be found or the
            "RAPID_API_KEY" or "RAPID_API_HOST" env vars don't exist
    
        If/when the hearthstone package is converted to a standalone library
        the process of requiring a user to create their own credentials and
        .env should be removed if possible

--- src/hearthstone/_api.py
+++ src/hearthstone/_api.py
@@ -6,19 +6,37 @@ GLOBALS:
         API_URI - Base URL for the Hearthstone API
         API_KEY - Key included in headers of requests to Hearthstone API
         API_HOST - Host URL included in headers of requests to Hearthstone API
+
+    A .env file must exist in the root of /hearthstone containing the 
+    RAPID_API_KEY and RAPID_API_HOST variables. Users should go to
+    'https://rapidapi.com/omgvamp/api/hearthstone/' and register to recieve
+    these values.
 """
 
 all = ['ENV']
 
-import os
+from os import environ, path
 from dotenv import load_dotenv
+from pathlib import Path
+from .errors import APIException
 
-load_dotenv()
+_env_file_path = str(Path(__file__).parent.resolve()) + "\.env"
+if not path.exists(_env_file_path):
+    raise APIException("Could not find .env file for hearthstone API. A "
+                        ".env file with 'RAPID_API_KEY', 'RAPID_API_HOST' in "
+                        " the root of the hearthstone package is missing.")
+else:
+    load_dotenv(dotenv_path=_env_file_path)
 
-ENV = {
-    "API_URI" : "https://omgvamp-hearthstone-v1.p.rapidapi.com",
-    "API_KEY" : os.getenv('RAPID_API_KEY'),
-    "API_HOST" : os.getenv('RAPID_API_HOST')
-}
+try:
+    ENV = {
+        "API_URI" : "https://omgvamp-hearthstone-v1.p.rapidapi.com",
+        "API_KEY" : environ["RAPID_API_KEY"],
+        "API_HOST" : environ["RAPID_API_HOST"],
+    }
+except KeyError as e:
+    raise APIException(f"{e} required but not found in .env")
+
+
 
 
